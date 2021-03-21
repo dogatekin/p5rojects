a21ef1d208050dd7bb6b3bbd7ab9971163df6c75
Doga Tekin <doga.tekin@epfl.ch>
2021-03-21 18:18:57 +0300

    Add random vectors

--- walker/walker.js
+++ walker/walker.js
@@ -1,12 +1,16 @@
 class Walker {
   constructor(x, y, mul=1) {
     this.pos = createVector(x, y)
+    this.vel = createVector(mul, 0)
     this.mul = mul
   }
 
   update() {
-    this.pos.x += random(-1, 1) * this.mul
-    this.pos.y += random(-1, 1) * this.mul
+    this.vel = p5.Vector.random2D().mult(this.mul)
+    this.pos.add(this.vel)
+    
+    this.pos.x = constrain(this.pos.x, 0, width)
+    this.pos.y = constrain(this.pos.y, 0, height)
   }
 
   show() {
