df9c1c253aa99eceffe95e5b2110926ae6b16e00
Doga Tekin <doga.tekin@epfl.ch>
2021-04-17 20:29:30 +0200

    More spirals

--- lituus-spiral/sketch.js
+++ lituus-spiral/sketch.js
@@ -5,16 +5,11 @@ function setup() {
 
 function draw() {
   background(180)
-  // background(0, 0, 50)
   noFill()
 
   size = 100
-  spiralWidth = 1
+  spiralWidth = 1000
   angle += 0.001
-
-  // if (mouseIsPressed) {
-  //   angle += 0.1
-  // }
 
   drawSpiral()
 }
@@ -23,13 +18,10 @@ function drawSpiral() {
   oldX = width / 2;
   oldY = height / 2;
 
-  // colorMode(HSB);
-  // stroke((1000*angle)%360, 100, 100)
-
   for (let i = 0; i < size; i++) {
     newAngle = angle * i;
-    x = (width / 2) + (spiralWidth * newAngle) * Math.sin(newAngle);
-    y = (height / 2) + (spiralWidth * newAngle) * Math.cos(newAngle);
+    x = (width / 2) + (spiralWidth / Math.sqrt(newAngle)) * Math.cos(newAngle);
+    y = (height / 2) + (spiralWidth / Math.sqrt(newAngle)) * Math.sin(newAngle);
 
     line(oldX, oldY, x, y);
     oldX = x;
