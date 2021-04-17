78a62721dc4753e0ef88f589b1ab5d44f1cf5790
Doga Tekin <doga.tekin@epfl.ch>
2021-04-17 16:00:00 +0200

    Add spiral

---
+++ spiral/sketch.js
@@ -0,0 +1,41 @@
+function setup() {
+  createCanvas(640, 360)
+  angle = 0
+}
+
+function draw() {
+  background(180)
+  // clear()
+  noFill()
+
+  size = 100
+  spiralWidth = 1
+  angle += 0.001
+
+  if (mouseIsPressed) {
+    angle += 0.1
+  }
+
+  drawSpiral()
+  // fill(0)
+  // text(angle, 10, 10)
+}
+
+function drawSpiral() {
+  oldX = width / 2;
+  oldY = height / 2;
+
+  for (let i = 0; i < size; i++) {
+    newAngle = angle*i;
+    x = (width / 2) + (spiralWidth * newAngle) * Math.sin(newAngle);
+    y = (height / 2) + (spiralWidth * newAngle) * Math.cos(newAngle);
+
+    // stroke(randomColor()); // Random Color for each line segment
+    // strokeWeight(randomWeight()); // Random Weight (1-5)
+
+    // ellipse(x, y, 1)
+    line(oldX, oldY, x, y);
+    oldX = x;
+    oldY = y;
+  }
+}
